68b16a121997fcaec4fb8ace69af4fc670c52c7b
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-08-21 14:56:51 +0500

    auth response fixed

--- lib/data/response.dart
+++ lib/data/response.dart
@@ -5,30 +5,15 @@ part 'response.g.dart';
 @JsonSerializable()
 class AuthResponse {
   final int id;
-  final String username;
   final String name;
-  final int limit;
-  final int sold;
-  @JsonKey(name: 'total_sold')
-  final int totalSold;
-  final int percent;
-  @JsonKey(name: 'seller_profit')
-  final int sellerProfit;
-  @JsonKey(name: 'admin_profit')
-  final int adminProfit;
+  final String username;
   @JsonKey(name: 'access_token')
   final String accessToken;
 
   AuthResponse({
     required this.id,
-    required this.username,
     required this.name,
-    required this.limit,
-    required this.sold,
-    required this.totalSold,
-    required this.percent,
-    required this.sellerProfit,
-    required this.adminProfit,
+    required this.username,
     required this.accessToken,
   });
 
--- lib/data/response.g.dart
+++ lib/data/response.g.dart
@@ -8,27 +8,15 @@ part of 'response.dart';
 
 AuthResponse _$AuthResponseFromJson(Map<String, dynamic> json) => AuthResponse(
       id: (json['id'] as num).toInt(),
-      username: json['username'] as String,
       name: json['name'] as String,
-      limit: (json['limit'] as num).toInt(),
-      sold: (json['sold'] as num).toInt(),
-      totalSold: (json['total_sold'] as num).toInt(),
-      percent: (json['percent'] as num).toInt(),
-      sellerProfit: (json['seller_profit'] as num).toInt(),
-      adminProfit: (json['admin_profit'] as num).toInt(),
+      username: json['username'] as String,
       accessToken: json['access_token'] as String,
     );
 
 Map<String, dynamic> _$AuthResponseToJson(AuthResponse instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'username': instance.username,
       'name': instance.name,
-      'limit': instance.limit,
-      'sold': instance.sold,
-      'total_sold': instance.totalSold,
-      'percent': instance.percent,
-      'seller_profit': instance.sellerProfit,
-      'admin_profit': instance.adminProfit,
+      'username': instance.username,
       'access_token': instance.accessToken,
     };
