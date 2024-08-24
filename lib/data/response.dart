c384d1b79508dc77228605d2c0695e21f3995b97
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-08-24 14:03:03 +0500

    profile response

--- lib/data/response.dart
+++ lib/data/response.dart
@@ -19,3 +19,33 @@ class AuthResponse {
 
   factory AuthResponse.fromJson(Map<String, dynamic> json) => _$AuthResponseFromJson(json);
 }
+
+@JsonSerializable()
+class SellerProfile {
+  final int id;
+  final String username;
+  final String name;
+  final int limit;
+  final int sold;
+  @JsonKey(name: 'total_sold')
+  final int totalSold;
+  final double percent;
+  @JsonKey(name: 'seller_profit')
+  final int sellerProfit;
+  @JsonKey(name: 'admin_profit')
+  final int adminProfit;
+
+  SellerProfile({
+    required this.id,
+    required this.username,
+    required this.name,
+    required this.limit,
+    required this.sold,
+    required this.totalSold,
+    required this.percent,
+    required this.sellerProfit,
+    required this.adminProfit,
+  });
+
+  factory SellerProfile.fromJson(Map<String, dynamic> json) => _$SellerProfileFromJson(json);
+}
--- lib/data/response.g.dart
+++ lib/data/response.g.dart
@@ -20,3 +20,29 @@ Map<String, dynamic> _$AuthResponseToJson(AuthResponse instance) =>
       'username': instance.username,
       'access_token': instance.accessToken,
     };
+
+SellerProfile _$SellerProfileFromJson(Map<String, dynamic> json) =>
+    SellerProfile(
+      id: (json['id'] as num).toInt(),
+      username: json['username'] as String,
+      name: json['name'] as String,
+      limit: (json['limit'] as num).toInt(),
+      sold: (json['sold'] as num).toInt(),
+      totalSold: (json['total_sold'] as num).toInt(),
+      percent: (json['percent'] as num).toDouble(),
+      sellerProfit: (json['seller_profit'] as num).toInt(),
+      adminProfit: (json['admin_profit'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$SellerProfileToJson(SellerProfile instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'name': instance.name,
+      'limit': instance.limit,
+      'sold': instance.sold,
+      'total_sold': instance.totalSold,
+      'percent': instance.percent,
+      'seller_profit': instance.sellerProfit,
+      'admin_profit': instance.adminProfit,
+    };
